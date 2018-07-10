1b9937f807f0ffc6d7c58ca774d732c68dd56e5d
Rafael Kayumov <justpoc@gmail.com>
2018-07-10 12:24:18 +0300

    Basic project setup

---
+++ Races/Source/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Races
+//
+//  Created by Rafael Kayumov on 10.07.2018.
+//  Copyright © 2018 Rafael Kayumov. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
